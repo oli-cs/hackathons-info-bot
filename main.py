2da6acfde316fc705349d21e06a2a9bb4a6a1d85
psyos9 <psyos9@exmail.nottingham.ac.uk>
2024-06-19 00:40:01 +0100

    updated formatting of information

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ def scrapeHackathonsUk(year:int) -> str:
     siteContents = requests.get("https://www.hackathons.org.uk/events/{currYear}".format(currYear=year))
     soup = BeautifulSoup(siteContents.content,"html.parser")
     rawText = soup.get_text()
-    return rawText
+    information = rawText.split(">>")
+    information[0] = information[0].split("Past Events")[1]
+    information = information[0:-1]
+    return str(information)
 
 load_dotenv()
 
@@ -22,7 +25,7 @@ hackBot = discord.Bot()
 async def hi(sourceChannel:discord.ApplicationContext):
     await sourceChannel.respond("Hi!")
 
-@hackBot.slash_command(name="hacks",desciption="returns scraped text from hackathons uk webpage")#does not work
+@hackBot.slash_command(name="hacks",desciption="returns scraped text from hackathons uk webpage")#works, but need to add explicit permission for channel
 async def hacks(sourceChannel:discord.ApplicationContext):
     await sourceChannel.respond(scrapeHackathonsUk(date.today().year))
 
