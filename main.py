89f90e8a704371301013bd9b44f16703583b257c
psyos9 <psyos9@exmail.nottingham.ac.uk>
2024-06-19 00:21:02 +0100

    hi works but hacks doesn't

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import date
-import re
+import discord
+import os
+from dotenv import load_dotenv
 
-def scrapeMajorLeagueHacking(year:int):
-    pass
+#def scrapeMajorLeagueHacking(year:int):
+#    pass
 
-def scrapeHackathonsUk(year:int) -> None:
+def scrapeHackathonsUk(year:int) -> str:
     siteContents = requests.get("https://www.hackathons.org.uk/events/{currYear}".format(currYear=year))
     soup = BeautifulSoup(siteContents.content,"html.parser")
     rawText = soup.get_text()
-    
+    return rawText
 
-        
-def main() -> None:
-    scrapeHackathonsUk(date.today().year)
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+hackBot = discord.Bot()
+
+@hackBot.slash_command(name="hi",description="testing")#works
+async def hi(sourceChannel:discord.ApplicationContext):
+    await sourceChannel.respond("Hi!")
+
+@hackBot.slash_command(name="hacks",desciption="returns scraped text from hackathons uk webpage")#does not work
+async def hacks(sourceChannel:discord.ApplicationContext):
+    await sourceChannel.respond(scrapeHackathonsUk(date.today().year))
+
+hackBot.run(os.getenv("TOKEN"))
